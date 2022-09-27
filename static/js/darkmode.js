cb30f8b7eb1ac88cc4387ad70025b07effdd597c
Undervis <cube@mail.ru>
2022-09-27 16:53:58 +0500

    dark mode

---
+++ static/js/darkmode.js
@@ -0,0 +1,7 @@
+$(document).ready(function (){
+    $('html').children().addClass('dark');
+    $('a').addClass('dark');
+    $('input').addClass('dark');
+    $('button').addClass('dark');
+    $('div').addClass('dark');
+})
