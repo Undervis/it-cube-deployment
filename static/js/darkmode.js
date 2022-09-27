4a340c17d681e86dae8ea9dd877e0f14494fb4f8
Undervis <cube@mail.ru>
2022-09-27 16:54:52 +0500

    dark mode

--- static/js/darkmode.js
+++ static/js/darkmode.js
@@ -1,7 +1,7 @@
-$(document).ready(function (){
+/*$(document).ready(function (){
     $('html').children().addClass('dark');
     $('a').addClass('dark');
     $('input').addClass('dark');
     $('button').addClass('dark');
     $('div').addClass('dark');
-})
+})*/
