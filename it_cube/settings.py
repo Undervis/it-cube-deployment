c6ce20d2cc6cc025fbbf5f7079ac1ed827b87eee
Undervis <undervis@undervis>
2023-03-13 02:24:31 +0500

    deployment settings

--- it_cube/settings.py
+++ it_cube/settings.py
@@ -81,12 +81,8 @@ WSGI_APPLICATION = 'it_cube.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'it_cube',
-        'USER': 'django',
-        'PASSWORD': 'Th398951',
-        'HOST': 'localhost',
-        'PORT': '3306'
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': 'it_cube.sqlite3'
     }
 }
 
